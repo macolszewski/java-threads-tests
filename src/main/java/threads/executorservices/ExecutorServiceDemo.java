e4b21267c5255245659c236aa55343b5128d81b1
macolszewski <olschevski@gmail.com>
2018-10-06 13:19:40 +0200

    Executors - multi threads

--- src/main/java/threads/executorservices/ExecutorServiceDemo.java
+++ src/main/java/threads/executorservices/ExecutorServiceDemo.java
@@ -1,9 +1,12 @@
 package threads.executorservices;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class ExecutorServiceDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TimeoutException, ExecutionException {
 //        ExecutorService executorService = Executors.newSingleThreadExecutor();
 //        executorService.submit(new Runnable() {
 //            @Override
@@ -36,25 +39,67 @@ public class ExecutorServiceDemo {
 //        executorService.shutdown();
 //        executorService2.shutdownNow();
 
-        ExecutorService executorServiceFuture = Executors.newSingleThreadExecutor();
-        Future<String> result = executorServiceFuture.submit(new Callable<String>() {
+//        ExecutorService executorServiceFuture = Executors.newSingleThreadExecutor();
+//        Future<String> result = executorServiceFuture.submit(new Callable<String>() {
+//            @Override
+//            public String call() throws Exception {
+//                Thread.sleep(2000);
+//                return "\u001B[32m"+ "Result of callable!"+"\u001B[0m";
+//            }
+//        });
+//        System.out.println("\u001B[31m"+"This operation is executed immediatley"+"\u001B[0m");
+//        try {
+//            executorServiceFuture.shutdown();
+//            System.out.println(result.get());
+//            System.out.println();
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        }
+
+
+        ExecutorService executorService3 = Executors.newFixedThreadPool(3);
+        List<Callable<String>> callables = new ArrayList<>();
+        callables.add(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Thread.sleep(1000);
+                return "Callable 1 execution";
+            }
+        });
+        callables.add(new Callable<String>() {
             @Override
             public String call() throws Exception {
                 Thread.sleep(2000);
-                return "Result of callable!";
+                return "Callable 2 excecution";
             }
         });
-        System.out.println("This operation is executed immediatley");
+        callables.add(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Thread.sleep(3000);
+                return "Callable 3 excecution";
+            }
+        });
         try {
-            executorServiceFuture.shutdown();
-            System.out.println(result.get());
-            System.out.println();
 
+            String result = executorService3.invokeAny(callables);
+            System.out.println(result);
+
+            List<Future<String >> futures = executorService3.invokeAll(callables);
+            for (Future<String> future : futures) {
+                try {
+                    System.out.println(future.get());
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
         }
+
 
     }
 }
